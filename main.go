314a20befa7815e8e729e4a6e16d76cf36f743ba
Mike Wiacek <mjwiacek@mjwiacek-macbookpro2.roam.corp.google.com>
2016-09-21 10:31:38 -0700

    Port to GAE

--- main.go
+++ main.go
@@ -2,108 +2,36 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Go-import-redirector is an HTTP server for a custom Go import domain.
+// Go-import-redirector-gae is an HTTP server for a custom Go import domain.
+// Unlike the original (located here: rsc.io/go-import-redirector) this version
+// runs on Google App Engine. Please set the values in the first var block to
+// the desired values.
+//
 // It responds to requests in a given import path root with a meta tag
 // specifying the source repository for the ``go get'' command and an
 // HTML redirect to the godoc.org documentation page for that package.
-//
-// Usage:
-//
-//	go-import-redirector [-addr address] [-tls] [-vcs sys] <import> <repo>
-//
-// Go-import-redirector listens on address (default ``:80'')
-// and responds to requests for URLs in the given import path root
-// with one meta tag specifying the given source repository for ``go get''
-// and another meta tag causing a redirect to the corresponding
-// godoc.org documentation page.
-//
-// For example, if invoked as:
-//
-//	go-import-redirector 9fans.net/go https://github.com/9fans/go
-//
-// then the response for 9fans.net/go/acme/editinacme will include these tags:
-//
-//	<meta name="go-import" content="9fans.net/go git https://github.com/9fans/go">
-//	<meta http-equiv="refresh" content="0; url=https://godoc.org/9fans.net/go/acme/editinacme">
-//
-// If both <import> and <repo> end in /*, the corresponding path element
-// is taken from the import path and substituted in repo on each request.
-// For example, if invoked as:
-//
-//	go-import-redirector rsc.io/* https://github.com/rsc/*
-//
-// then the response for rsc.io/x86/x86asm will include these tags:
-//
-//	<meta name="go-import" content="rsc.io/x86 git https://github.com/rsc/x86">
-//	<meta http-equiv="refresh" content="0; url=https://godoc.org/rsc.io/x86/x86asm">
-//
-// Note that the wildcard element (x86) has been included in the Git repo path.
-//
-// The -addr option specifies the HTTP address to serve (default ``:http'').
-//
-// The -tls option causes go-import-redirector to serve HTTPS on port 443,
-// loading an X.509 certificate and key pair from files in the current directory
-// named after the host in the import path with .crt and .key appended
-// (for example, rsc.io.crt and rsc.io.key).
-// Like for http.ListenAndServeTLS, the certificate file should contain the
-// concatenation of the server's certificate and the signing certificate authority's certificate.
-//
-// The -vcs option specifies the version control system, git, hg, or svn (default ``git'').
-//
-// Deployment on Google Cloud Platform
-//
-// For the case of a redirector for an entire domain (such as rsc.io above),
-// the Makefile in this directory contains recipes to deploy a trivial VM running
-// just this program, using a static IP address that can be loaded into the
-// DNS configuration for the target domain.
-//
-package main
+package go_import_redirector_gae
 
 import (
 	"bytes"
-	"flag"
-	"fmt"
 	"html/template"
-	"log"
 	"net/http"
-	"os"
 	"strings"
 )
 
 var (
-	addr       = flag.String("addr", ":http", "serve http on `address`")
-	serveTLS   = flag.Bool("tls", false, "serve https on :443")
-	vcs        = flag.String("vcs", "git", "set version control `system`")
-	importPath string
-	repoPath   string
+	vcs        = "git"
+	importPath = "m8k.in/*"
+	repoPath   = "https://github.com/mikewiacek/*"
 	wildcard   bool
 )
 
-func usage() {
-	fmt.Fprintf(os.Stderr, "usage: go-import-redirector <import> <repo>\n")
-	fmt.Fprintf(os.Stderr, "options:\n")
-	flag.PrintDefaults()
-	fmt.Fprintf(os.Stderr, "examples:\n")
-	fmt.Fprintf(os.Stderr, "\tgo-import-redirector rsc.io/* https://github.com/rsc/*\n")
-	fmt.Fprintf(os.Stderr, "\tgo-import-redirector 9fans.net/go https://github.com/9fans/go\n")
-	os.Exit(2)
-}
-
-func main() {
-	log.SetFlags(0)
-	log.SetPrefix("go-import-redirector: ")
-	flag.Usage = usage
-	flag.Parse()
-	if flag.NArg() != 2 {
-		flag.Usage()
-	}
-	importPath = flag.Arg(0)
-	repoPath = flag.Arg(1)
+func init() {
 	if !strings.Contains(repoPath, "://") {
-		log.Fatal("repo path must be full URL")
+		panic("repo path must be full URL")
 	}
 	if strings.HasSuffix(importPath, "/*") != strings.HasSuffix(repoPath, "/*") {
-		log.Fatal("either both import and repo must have /* or neither")
+		panic("either both import and repo must have /* or neither")
 	}
 	if strings.HasSuffix(importPath, "/*") {
 		wildcard = true
@@ -111,16 +39,6 @@ func main() {
 		repoPath = strings.TrimSuffix(repoPath, "/*")
 	}
 	http.HandleFunc(strings.TrimSuffix(importPath, "/")+"/", redirect)
-	if *serveTLS {
-		host := importPath
-		if i := strings.Index(host, "/"); i >= 0 {
-			host = host[:i]
-		}
-		go func() {
-			log.Fatal(http.ListenAndServeTLS(":https", host+".crt", host+".key", nil))
-		}()
-	}
-	log.Fatal(http.ListenAndServe(*addr, nil))
 }
 
 var tmpl = template.Must(template.New("main").Parse(`<!DOCTYPE html>
@@ -172,7 +90,7 @@ func redirect(w http.ResponseWriter, req *http.Request) {
 	}
 	d := &data{
 		ImportRoot: importRoot,
-		VCS:        *vcs,
+		VCS:        vcs,
 		VCSRoot:    repoRoot,
 		Suffix:     suffix,
 	}
